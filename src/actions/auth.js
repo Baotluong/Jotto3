dfde478d68c6096035483dc7a27e4c0e93dabe61
Bao Luong <baotluong@gmail.com>
2019-09-27 17:25:17 -0400

    removing firebase from client

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,4 +1,4 @@
-import { firebase, googleAuthProvider } from '../firebase/firebase';
+// import { firebase, googleAuthProvider } from '../firebase/firebase';
 import { history } from '../routers/AppRouter';
 
 export const login = (uid) => ({
@@ -6,21 +6,21 @@ export const login = (uid) => ({
   uid
 });
 
-export const startLogin = () => {
-  return () => {
-    return firebase.auth().signInWithPopup(googleAuthProvider);
-  };
-};
+// export const startLogin = () => {
+//   return () => {
+//     return firebase.auth().signInWithPopup(googleAuthProvider);
+//   };
+// };
 
 export const logout = () => ({
   type: 'LOGOUT'
 });
 
-export const startLogout = () => {
-  return () => {
-    return firebase.auth().signOut();
-  };
-};
+// export const startLogout = () => {
+//   return () => {
+//     return firebase.auth().signOut();
+//   };
+// };
 
 export const setError = (error) => ({
   type: 'SET_ERROR',
--- src/app.js
+++ src/app.js
@@ -7,8 +7,6 @@ import { login, logout } from './actions/auth';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
-import { firebase } from './firebase/firebase';
-import LoadingPage from './components/LoadingPage';
 import uuid from 'uuid';
 
 const store = configureStore();
