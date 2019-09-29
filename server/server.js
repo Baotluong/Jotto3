2992c1d4dd267215405b38a5d984d5855f5e270b
Bao Luong <baotluong@gmail.com>
2019-09-29 18:09:30 -0400

    trying something else

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const path = require('path');
-// const dotenv = require('dotenv').config();
+const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
